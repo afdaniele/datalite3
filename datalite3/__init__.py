81bbfbd749db1d52c287bcadeb141a79485c423d
Andrea F. Daniele <afdaniele@ttic.edu>
2022-01-20 14:50:32 -0600

    Bump version: 1.0.0 → 1.0.1

--- datalite3/__init__.py
+++ datalite3/__init__.py
@@ -2,7 +2,7 @@ from .constraints import Primary, Unique
 from .decorator import datalite
 from .commons import DataLiteClass
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = [
     # modules
--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Andrea F. Daniele'
 author = 'Andrea F. Daniele'
 
 # The full version, including alpha/beta/rc tags
-release = 'vv1.0.0'
+release = 'vv1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
